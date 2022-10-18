c73965af9162149d94d1cf3c1fd0d7646b2fc06e
Thisuru Deesan <thisuru.d@eyepax.com>
2022-10-18 11:35:36 +0530

    Client APIs wrapped with catchAsync

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -7,32 +7,25 @@ const { getAll,
 } = require('../services/clientService');
 const AppError = require('../util/errorHandler/appError');
 const catchAsync = require('../util/errorHandler/catchAsync');
-const { userServerError } = require('../util/errorHandler/userServerError');
 
 //get all Clients
-const clientGetAll = async (req, res) => {
-    try {
-        const params = req.query
-        const { data, count } = await getAll(params)
-        const response = {
-            clients: data.map(client => ({
-                id: client._id,
-                firstname: client.firstname,
-                lastname: client.lastname,
-                phonenumber: client.phonenumber,
-                email: client.email
-            }
-            )),
-            totalPages: count,
-            currentPage: params?.page
+const clientGetAll = catchAsync(async (req, res, next) => {
+    const params = req.query
+    const { data, count } = await getAll(params)
+    const response = {
+        clients: data.map(client => ({
+            id: client._id,
+            firstname: client.firstname,
+            lastname: client.lastname,
+            phonenumber: client.phonenumber,
+            email: client.email
         }
-
-        res.send(response)
-    } catch (error) {
-        console.log(error);
-        res.status(400).send({ message: error.message || "Error Occurred while retriving user information" })
+        )),
+        totalPages: count,
+        currentPage: params?.page
     }
-}
+    res.send(response)
+})
 
 //get the selected Client based on Params id 
 const clientGetSingleClient = catchAsync(async (req, res, next) => {
@@ -46,27 +39,17 @@ const clientGetSingleClient = catchAsync(async (req, res, next) => {
 })
 
 //Client create post API call (Save form data in db)
-const clientCreatePost = async (req, res) => {
+const clientCreatePost = catchAsync(async (req, res, next) => {
     const { firstname, lastname, phonenumber, email } = req.body
+    const user = await getClientByEmail(email)
 
-    try {
-        const user = await getClientByEmail(email)
-
-        if (user) {
-            return res.status(203).json({
-                status: false,
-                message: 'This email is already in use.'
-            });
-        } else {
-            const result = await createPost({ firstname, lastname, phonenumber, email })
-            res.send(result)
-        }
-
-    } catch (error) {
-        const errors = { firstname: '', lastname: '', username: '', phonenumber: '' };
-        userServerError(error, errors, 'Client', res)
+    if (user) {
+        throw new AppError('This email is already in use.', 203);
+    } else {
+        const result = await createPost({ firstname, lastname, phonenumber, email })
+        res.send(result)
     }
-}
+})
 
 //delete selected Client 
 const clientDelete = catchAsync(async (req, res, next) => {
@@ -80,34 +63,21 @@ const clientDelete = catchAsync(async (req, res, next) => {
 })
 
 //update selected client
-const clientUpdate = async (req, res) => {
+const clientUpdate = catchAsync(async (req, res) => {
     const id = req.params.id;
+    const user = await getClientByEmail(req.body.email)
 
-    try {
-        const id = req.params.id;
-
-        const user = await getClientByEmail(req.body.email)
-
-        if (user && (id != user.id)) {
-            return res.status(203).json({
-                status: false,
-                message: 'This email is already in use.'
-            });
+    if (user && (id != user.id)) {
+        throw new AppError('This email is already in use.', 203);
+    } else {
+        const result = await updateClient(id, req.body)
+        if (!result) {
+            throw new AppError('Cannot Update client. Maybe client not found!', 404);
         } else {
-            // const result = await Client.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
-            const result = await updateClient(id, req.body)
-            if (!result) {
-                res.status(404).send({ message: `Cannot Update client with ${id}. Maybe client not found!` })
-            } else {
-                res.send(result)
-            }
+            res.send(result)
         }
-
-    } catch (error) {
-        console.log(error);
-        res.status(400).send({ message: "Error Updating client information" })
     }
-}
+})
 
 module.exports = {
     clientGetAll,
--- util/errorHandler/errorController.js
+++ util/errorHandler/errorController.js
@@ -1,5 +1,5 @@
 const sendErrorDev = (err, res) => {
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || 400;
     res.status(statusCode).json({
         success: 0,
         message: err.message,
