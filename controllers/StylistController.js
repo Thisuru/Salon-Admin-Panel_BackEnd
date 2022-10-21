cbfc4dcba1a139f808bd31e0cdf358a2defb5e25
Thisuru Deesan <thisuru.d@eyepax.com>
2022-10-21 12:40:28 +0530

    Stylist controller APIs wrapped with catchAsync

--- controllers/StylistController.js
+++ controllers/StylistController.js
@@ -2,44 +2,28 @@ const Stylist = require('../models/stylist');
 const { getReservedStylishIds, getThisWeekReservationIds, getSpecificFieldsById, getTimeDifference } = require("../services/reservationService");
 const { getAvailableFromResevedIds, getSingle } = require("../services/stylishService");
 var moment = require('moment');
+const catchAsync = require('../util/errorHandler/catchAsync');
+const AppError = require('../util/errorHandler/appError');
 
 //get all Stylists
-const stylistGetAll = async (req, res) => {
-    try {
-        const result = await Stylist.find().sort({ createdAt: -1 })
-        res.send(result)
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message || "Error Occurred while retriving user information" })
-    }
-}
+const stylistGetAll = catchAsync(async (req, res, next) => {
+    const result = await Stylist.find().sort({ createdAt: -1 })
+    res.send(result)
+})
 
 //Stylists create post API call (Save form data in db)
-const stylistCreatePost = async (req, res) => {
+const stylistCreatePost = catchAsync(async (req, res) => {
+    const client = new Stylist({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        phonenumber: req.body.phonenumber,
+        email: req.body.email
+    });
+    const result = await client.save(client)
+    res.send(result)
+})
 
-    if (!req.body) {
-        res.status(400).send({ message: "Content can not be empty!" });
-        return;
-    }
-
-    try {
-        const client = new Stylist({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            phonenumber: req.body.phonenumber,
-            email: req.body.email
-        });
-
-        const result = await client.save(client)
-        res.send(result)
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message || "Error Update user information" })
-    }
-}
-
-const getAvailableStylish = async (req, res) => {
+const getAvailableStylish = async (req, res, next) => {
     try {
         const start = new Date(req.body.start); //"2022-09-18T09:15:55.970Z"
         const end = new Date(req.body.end); //"2022-09-18T09:15:55.970Z"
@@ -54,9 +38,7 @@ const getAvailableStylish = async (req, res) => {
 
         res.send({ availabelStylish, resevedStylishIds });
     } catch (error) {
-        res
-            .status(500)
-            .send({ message: error.message || "Error Occurred while retriving user information" });
+        next(error)
     }
 };
 
@@ -74,7 +56,7 @@ const getEachStylistTimePerWeek = async (req, res) => {
             let start = reservation[0].startTime;
             let end = reservation[0].endTime
             let minutesDifference = getTimeDifference(start, end);
-            
+
             middleReservationObj.name = reservation[0].stylists.firstname
             middleReservationObj.WeeklyTimeInMins = minutesDifference
 
@@ -85,50 +67,41 @@ const getEachStylistTimePerWeek = async (req, res) => {
         console.log("FINAL: ", getSingleReservation);
 
         var holder = {};
-        
-        getSingleReservation.forEach(function(d) {
-          if (holder.hasOwnProperty(d.name)) {
-            holder[d.name] = holder[d.name] + d.WeeklyTimeInMins;
-          } else {
-            holder[d.name] = d.WeeklyTimeInMins;
-          }
+
+        getSingleReservation.forEach(function (d) {
+            if (holder.hasOwnProperty(d.name)) {
+                holder[d.name] = holder[d.name] + d.WeeklyTimeInMins;
+            } else {
+                holder[d.name] = d.WeeklyTimeInMins;
+            }
         });
-        
+
         var barchartReservationData = [];
-        
+
         for (var prop in holder) {
-          barchartReservationData.push({ name: prop, WeeklyTimeInMins: holder[prop] });
+            barchartReservationData.push({ name: prop, WeeklyTimeInMins: holder[prop] });
         }
-        
+
         console.log(barchartReservationData);
 
         res.send(barchartReservationData)
 
     } catch (error) {
-        res
-            .status(500)
-            .send({ message: error.message || "Error Occurred while retriving stylist information" });
+        next(error)
     }
 }
 
 //get the selected Stylist based on Params id 
-const getStylistNameById = async (req, res) => {
+const getStylistNameById = catchAsync(async (req, res, next) => {
+    const id = req.params.id;
+    const result = await getSingle(id)
 
-    try {
-        const id = req.params.id;
-        const result = await getSingle(id)
-
-        if (!result) {
-            res.status(404).send({ message: `Not found user with id ${id}` })
-        } else {
-            res.send(result)
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: `Erro retrieving user with id= ${id}` })
+    if (!result) {
+        throw new AppError('User not found!', 404);
+    } else {
+        res.send(result)
     }
-}
-
+})
 
 module.exports = {
     stylistGetAll,
